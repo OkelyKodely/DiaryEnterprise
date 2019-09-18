4b0388f8d6dc31c9bdd1ea34821f4bb2ef181e72
Daniel Cho <dh.cho428@gmail.com>
2019-09-18 07:30:49 -0700

    Add files via upload

---
+++ Diary.Designer.vb
@@ -0,0 +1,197 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Diary
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(552, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "New"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(13, 92)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(301, 218)
+        Me.RichTextBox1.TabIndex = 1
+        Me.RichTextBox1.Text = ""
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.RichTextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox2.ForeColor = System.Drawing.SystemColors.Window
+        Me.RichTextBox2.Location = New System.Drawing.Point(326, 77)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(301, 233)
+        Me.RichTextBox2.TabIndex = 2
+        Me.RichTextBox2.Text = ""
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(45, 42)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(240, 21)
+        Me.ComboBox1.TabIndex = 3
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(13, 317)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Update"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(552, 317)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Save"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(379, 42)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(141, 20)
+        Me.TextBox1.TabIndex = 6
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(162, 316)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "Prev"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(239, 317)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 8
+        Me.Button5.Text = "Next"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(91, 12)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(146, 23)
+        Me.Button6.TabIndex = 9
+        Me.Button6.Text = "Add BULK XML Entries"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.Location = New System.Drawing.Point(91, 69)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(95, 18)
+        Me.Button7.TabIndex = 10
+        Me.Button7.Text = "Backup to XML File"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(379, 316)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(75, 23)
+        Me.Button8.TabIndex = 11
+        Me.Button8.Text = "Reset"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(13, 12)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(75, 23)
+        Me.Button9.TabIndex = 12
+        Me.Button9.Text = "Exit"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(657, 376)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.RichTextBox2)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Diary"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents RichTextBox2 As RichTextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+End Class
---
+++ Diary.vb
@@ -0,0 +1,151 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Imports System.IO
+Imports System.Xml
+
+Public Class Diary
+
+    Public Sub New()
+        InitializeComponent()
+        Call PrepareDatas()
+        ComboBox1.SelectedIndex = 0
+        ComboBox1_SelectedIndexChanged(Nothing, Nothing)
+    End Sub
+
+    Private Sub PrepareDatas()
+        Dim cmd As SqlCommand
+        Dim con = New SqlConnection("server = localhost; database = test; integrated security = true")
+        Dim reader As SqlDataReader
+        Call con.Open()
+        cmd = New SqlCommand("select name from diary", con)
+        reader = cmd.ExecuteReader()
+        Call ComboBox1.Items.Clear()
+        While reader.Read()
+            ComboBox1.Items.Add(reader.GetValue(0).ToString)
+        End While
+        con.Close()
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        RichTextBox2.Focus()
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Dim cmd As SqlCommand
+        Dim con = New SqlConnection("server = localhost; database = test; integrated security = true")
+        Dim reader As SqlDataReader
+        Call con.Open()
+        Dim a = ComboBox1.SelectedItem.ToString
+        cmd = New SqlCommand("select name, content from diary where name like '" & a.Replace("'", "''") & "'", con)
+        reader = cmd.ExecuteReader()
+        If reader.Read() Then
+            RichTextBox1.Text = reader.GetValue(1).ToString
+        End If
+        con.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim cmd As SqlCommand
+        Dim con = New SqlConnection("server = localhost; database = test; integrated security = true")
+        Call con.Open()
+        cmd = New SqlCommand("update diary set content = '" & RichTextBox1.Text.Replace("'", "''") & "' where name = '" & ComboBox1.SelectedItem.ToString.Replace("'", "''") & "'", con)
+        Call cmd.ExecuteNonQuery()
+        con.Close()
+        MessageBox.Show("Updated")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim cmd As SqlCommand
+        Dim con = New SqlConnection("server = localhost; database = test; integrated security = true")
+        Call con.Open()
+        cmd = New SqlCommand("insert into diary (name, content) values ('" & TextBox1.Text.Replace("'", "''") & "', '" & RichTextBox2.Text.Replace("'", "''") & "')", con)
+        Call cmd.ExecuteNonQuery()
+        con.Close()
+        PrepareDatas()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Try
+            ComboBox1.SelectedIndex = ComboBox1.SelectedIndex - 1
+            ComboBox1_SelectedIndexChanged(Nothing, Nothing)
+        Catch Ex As Exception
+        End Try
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Try
+            ComboBox1.SelectedIndex = ComboBox1.SelectedIndex + 1
+            ComboBox1_SelectedIndexChanged(Nothing, Nothing)
+        Catch Ex As Exception
+        End Try
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Try
+            Dim OpenFileDialog1 As New OpenFileDialog
+            OpenFileDialog1.Title = "Open File..."
+            OpenFileDialog1.Multiselect = False
+            OpenFileDialog1.Filter = "All Files|*.*"
+            OpenFileDialog1.ShowDialog()
+            Dim a = File.ReadAllText(OpenFileDialog1.FileName)
+            Dim xml As XDocument = XDocument.Parse(a)
+            Dim listRows As List(Of XElement) = xml.Descendants("title").ToList()
+            For Each node As XElement In listRows
+                Dim values As List(Of XElement) = node.Descendants("content").ToList()
+                Dim ip As String = values(0)
+                Dim bytes As String = values(1)
+                Dim cmd As SqlCommand
+                Dim con = New SqlConnection("server = localhost; database = test; integrated security = true")
+                Call con.Open()
+                Try
+                    cmd = New SqlCommand("insert into diary (name, content) values ('" & ip.Replace("'", "''") & "', '" & bytes.Replace("'", "''") & "')", con)
+                    Call cmd.ExecuteNonQuery()
+                Catch Ex As Exception
+                End Try
+                con.Close()
+                PrepareDatas()
+            Next
+            ComboBox1.SelectedIndex = 0
+            ComboBox1_SelectedIndexChanged(Nothing, Nothing)
+        Catch Ex As Exception
+        End Try
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        RichTextBox2.Text = ""
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim myDoc As Xml.XmlDocument = New Xml.XmlDocument
+        Dim docNode = myDoc.CreateXmlDeclaration("1.0", "UTF-8", Nothing)
+        Dim productsNode = myDoc.CreateElement("datas")
+        Dim root As XmlNode = myDoc.DocumentElement
+        myDoc.AppendChild(docNode)
+        myDoc.AppendChild(productsNode)
+        For i = 0 To ComboBox1.Items.Count - 1
+            Dim myTitle As XmlElement = myDoc.CreateElement("title")
+            Dim myContent1 As XmlElement = myDoc.CreateElement("content")
+            Dim myContent2 As XmlElement = myDoc.CreateElement("content")
+            myTitle.InnerText = ComboBox1.Items(i).ToString
+            Dim cmd As SqlCommand
+            Dim con = New SqlConnection("server = localhost; database = test; integrated security = true")
+            Call con.Open()
+            cmd = New SqlCommand("select name, content from diary where name like '" & ComboBox1.Items(i).ToString.Replace("'", "''") & "'", con)
+            Dim reader As SqlDataReader
+            reader = cmd.ExecuteReader()
+            If reader.Read() Then
+                myContent1.InnerText = reader.GetValue(0).ToString()
+                myContent2.InnerText = reader.GetValue(1).ToString()
+            End If
+            Call con.Close()
+            productsNode.AppendChild(myTitle)
+            myTitle.AppendChild(myContent1)
+            myTitle.AppendChild(myContent2)
+        Next
+        myDoc.Save("D:\\xml.txt")
+        MsgBox("Xml File GeneratedUpdated")
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Application.Exit()
+    End Sub
+End Class
